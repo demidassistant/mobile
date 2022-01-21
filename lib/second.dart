79b327c615e6b59067fde89bc33bef5067a36511
Vladislav Osin <osinvladislav@yandex.ru>
2022-01-21 11:29:04 +0300

    Second commit

---
+++ lib/second.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotHomePage extends StatefulWidget {
+  const NotHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<NotHomePage> createState() => _NotHomePageState();
+}
+
+class _NotHomePageState extends State<NotHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("privet");
+  }
+}
